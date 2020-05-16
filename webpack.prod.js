22da770ae5619c32dc93307f765e1d73ae8f9e34
Piotr Praga <piotr.praga@webfleet.com>
2020-05-16 16:34:51 +0200

    fix clean-webpack-plugin named import

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
@@ -10,7 +10,9 @@ module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ['dist']
+        }),
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].css'
         }),
